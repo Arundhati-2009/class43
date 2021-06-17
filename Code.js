a0bdde805fd0d5f59c7e41abb5475cf836a0e55a
Arundhati-2009 <76554711+Arundhati-2009@users.noreply.github.com>
2021-06-17 15:07:06 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,27 @@
+const accessCode1="EGG";
+const accessCode2="CANDLE";
+const accessCode3="ALLOFTHEM";
+const accessCode4="SPONGE";
+const accessCode5="AREYOUASLEEPYET?";
+const accessCode6="THEFUTURE";
+
+function clues(){
+    fill("skyblue");
+    textSize(15);
+    text("G E G",100,70);
+    text("Hint:What has to be broken before you can use it?",100,90);
+    fill("skyblue");
+    textSize(15);
+    text("D C L N E A",500,70);
+    text("Hint:I’m tall when I’m young, and I’m short when I’m old. What am I?",500,90);
+    text("H E T L M F O L A",100,170);
+    text("Hint:What month of the year has 28 days?",100,190);
+    text("P G N S O E",500,170);
+    text("Hint: What is full of holes but still holds water?",500,190);
+    text("T R P E E A L S A O Y U Y E E",100,280);
+    text("Hint: What question can you never answer yes to?",100,300);
+    text("R T F T H U E U E ",500,280);
+    text("Hint: What is always in front of you but can’t be seen?",500,300);
+
+
+}
